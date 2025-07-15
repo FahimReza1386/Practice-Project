69722539d66c0e3c7495e4dcc8f6120598358275
FahimReza <Fahimreza20200@gmail.com>
2025-07-15 11:49:20 +0330

    Create The Contact Page

--- core/website/views.py
+++ core/website/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "website/home.html"
     
+class ContactView(TemplateView):
+    template_name = "website/contact.html"
+    
