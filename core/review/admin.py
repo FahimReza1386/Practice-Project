26c5be07985e3be9e3f34fd5ddc7eea33af28eb4
FahimReza <Fahimreza20200@gmail.com>
2025-07-19 09:32:47 +0330

    Create The Model and Admin For Review App

--- core/review/admin.py
+++ core/review/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ReviewModel, ReviewStatusModel
 # Register your models here.
+
+@admin.register(ReviewModel)
+class ReviewModelAdmin(admin.ModelAdmin):
+    list_display = ("user__first_name", "blog__title", "pk", "status")
+    list_filter= ("status",)
+    search_fields= ("user__first_name", "blog__title")
--- core/review/models.py
+++ core/review/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from ckeditor_uploader.fields import RichTextUploadingField
+from shop.models import BlogModel
 
+User = get_user_model()
 # Create your models here.
+
+class ReviewStatusModel(models.IntegerChoices):
+    pending= 1, ("در انتظار تایید")
+    accepted= 2, ("تایید شده")
+    rejected= 3, ("لغو شده")
+
+
+class ReviewModel(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    blog= models.ForeignKey(BlogModel, on_delete=models.CASCADE)
+    description=RichTextUploadingField(max_length=300)
+    rate= models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=1)
+    status= models.IntegerField(choices=ReviewStatusModel.choices, default=ReviewStatusModel.pending.value)
+
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date= models.DateTimeField(auto_now=True)
+
+    def __str__(self):  
+        return f"{self.user.first_name}-{self.blog.title}"
